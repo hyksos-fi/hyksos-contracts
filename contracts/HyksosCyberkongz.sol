aab1d149bfe445dc688f95c07dc6fa61a53d8778
fylkir <fylkirh@protonmail.com>
2022-01-19 22:36:19 +0100

    Final repo refactor & cleanup

--- contracts/HyksosCyberkongz.sol
+++ contracts/HyksosCyberkongz.sol
@@ -5,56 +5,16 @@ import '../openzeppelin/token/ERC721/IERC721Receiver.sol';
 import '../openzeppelin/token/ERC20/IERC20.sol';
 import '../openzeppelin/utils/math/SafeMath.sol';
 
+import './DepositQueue.sol';
+import './IHyksos.sol';
+
 interface IKongz is IERC721 {
     function balanceOG(address _user) external view returns(uint256);
     function getReward() external;
 }
 
-
-contract DepositQueue {
-    struct Deposit {
-        uint256 amount;
-        address sender;
-    }
-
-    Deposit[] depositQueue;
-    uint256 topIndex;
-
-    function isDepositQueueEmpty() internal view returns(bool) {
-        return depositQueue.length <= topIndex;
-    }
-
-    modifier nonEmpty() {
-        require(!isDepositQueueEmpty());
-        _;
-    }
-
-    function pushDeposit(uint256 _amount, address _sender) internal {
-        depositQueue.push(Deposit(_amount, _sender));
-    }
-
-    function popDeposit() internal nonEmpty returns(Deposit memory) {
-        delete depositQueue[topIndex];
-        topIndex++;
-    }
-
-    function getTopDeposit() internal nonEmpty view returns(Deposit memory) {
-        return depositQueue[topIndex];
-    }
-
-    function setTopDepositAmount(uint256 _amount) internal nonEmpty {
-        depositQueue[topIndex].amount = _amount;
-    }
-}
-
-
-contract BananaPool is DepositQueue {
+contract HyksosCyberkongz is IHyksos, DepositQueue {
     using SafeMath for uint256;
-
-    event BananaDeposit(address indexed addr, uint256 value);
-    event BananaWithdrawal(address indexed addr, uint256 value);
-    event KongDeposit(address indexed addr, uint256 id);
-    event KongWithdrawal(address indexed addr, uint256 id);
     
     IKongz kongz;
     IERC20 bananas;
@@ -62,18 +22,18 @@ contract BananaPool is DepositQueue {
     struct Kong {
         uint256 timeDeposited;
         address owner;
-        Deposit[] lenders;
+        Deposit[] shareholders;
     }
 
     mapping(address => uint256) bananaBalance;
     mapping(uint256 => Kong) depositedKongs;
     uint256 totalBananasBalance;
 
-    uint256 constant DEPOSIT_LENGTH_DAYS = 10;
+    uint256 constant DEPOSIT_LENGTH_DAYS = 10; // TBD
     uint256 constant DEPOSIT_LENGTH_SECONDS = DEPOSIT_LENGTH_DAYS * 86400;
     uint256 constant BASE_RATE = 10 ether;
-    uint256 constant MIN_DEPOSIT = 1 * BASE_RATE; // do ustalenia
-    uint256 constant ROI_PCTG = 80;
+    uint256 constant MIN_DEPOSIT = 1 * BASE_RATE; // TBD
+    uint256 constant ROI_PCTG = 80; // TBD
     uint256 constant KONG_WORK_VALUE = BASE_RATE * DEPOSIT_LENGTH_DAYS;
     uint256 constant LOAN_AMOUNT = KONG_WORK_VALUE * ROI_PCTG / 100;
 
@@ -83,55 +43,54 @@ contract BananaPool is DepositQueue {
         bananas = IERC20(_bananas);
     }
 
-    function depositBananas(uint256 _amount) external {
-        require(_amount > 0, "Amount must be positive");
-        require(bananas.balanceOf(msg.sender) >= _amount, "Not enough bananas to deposit");
-        // possibly add allowance for the whole amount
+    function depositErc20(uint256 _amount) external override {
+        require(_amount > 0, "Amount must be positive.");
+        require(bananas.balanceOf(msg.sender) >= _amount, "Not enough bananas to deposit.");
         uint256 allowance = bananas.allowance(msg.sender, address(this));
         require(allowance >= _amount, "Not enough allowance");
         bananas.transferFrom(msg.sender, address(this), _amount);
         bananaBalance[msg.sender] += _amount;
         pushDeposit(_amount, msg.sender);
         totalBananasBalance += _amount;
-        emit BananaDeposit(msg.sender, _amount);
+        emit Erc20Deposit(msg.sender, _amount);
     }
 
-    function withdrawBananas() external {
-        require(bananaBalance[msg.sender] > 0, "No bananas to withdraw");
+    function withdrawErc20() external override {
+        require(bananaBalance[msg.sender] > 0, "No bananas to withdraw.");
         uint256 amount = bananaBalance[msg.sender];
         bananas.transfer(msg.sender, amount);
         totalBananasBalance -= amount;
         bananaBalance[msg.sender] = 0;
-        emit BananaWithdrawal(msg.sender, amount);
+        emit Erc20Withdrawal(msg.sender, amount);
     }
 
-    function lendKong(uint256 _id) external {
-        require(kongz.ownerOf(_id) == msg.sender, "Not the Kong owner");
+    function depositNft(uint256 _id) external override {
+        require(kongz.ownerOf(_id) == msg.sender, "Not the Kong owner.");
         require(kongz.getApproved(_id) == address(this));
         kongz.transferFrom(msg.sender, address(this), _id);
         bananas.transfer(msg.sender, LOAN_AMOUNT);
         depositedKongs[_id].timeDeposited = block.timestamp;
         depositedKongs[_id].owner = msg.sender;
-        selectLenders(_id);
-        emit KongDeposit(msg.sender, _id);
+        selectShareholders(_id);
+        emit NftDeposit(msg.sender, _id);
     }
 
-    function withdrawKong(uint256 _id) external {
-        require(depositedKongs[_id].owner == msg.sender, "Not the kong owner");
-        require(depositedKongs[_id].timeDeposited.add(DEPOSIT_LENGTH_SECONDS) < block.timestamp, "Too early to withdraw");
+    function withdrawNft(uint256 _id) external override {
+        require(depositedKongs[_id].owner == msg.sender, "Not the kong owner.");
+        require(depositedKongs[_id].timeDeposited.add(DEPOSIT_LENGTH_SECONDS) < block.timestamp, "Too early to withdraw.");
         uint256 reward = calcReward(block.timestamp.sub(depositedKongs[_id].timeDeposited));
         kongz.getReward();
-        for (uint i = 0; i < depositedKongs[_id].lenders.length; i++) {
-            Deposit memory d = depositedKongs[_id].lenders[i];
+        for (uint i = 0; i < depositedKongs[_id].shareholders.length; i++) {
+            Deposit memory d = depositedKongs[_id].shareholders[i];
             uint256 payback = d.amount.mul(100).div(ROI_PCTG).mul(reward).div(KONG_WORK_VALUE);
             bananas.transfer(d.sender, payback);
         }
         kongz.transferFrom(address(this), depositedKongs[_id].owner, _id);
         delete depositedKongs[_id];
-        emit KongWithdrawal(msg.sender, _id);
+        emit NftWithdrawal(msg.sender, _id);
     }
 
-    function selectLenders(uint256 _id) internal {
+    function selectShareholders(uint256 _id) internal {
         require(totalBananasBalance >= LOAN_AMOUNT, "Not enough bananas to fund a loan.");
         uint256 selectedAmount = 0;
         while (!isDepositQueueEmpty()) {
@@ -151,12 +110,12 @@ contract BananaPool is DepositQueue {
                 uint256 usedAmount = LOAN_AMOUNT.sub(selectedAmount);
                 uint256 leftAmount = depositAmount.sub(usedAmount);
                 setTopDepositAmount(leftAmount);
-                depositedKongs[_id].lenders.push(Deposit(usedAmount, d.sender));
+                depositedKongs[_id].shareholders.push(Deposit(usedAmount, d.sender));
                 bananaBalance[d.sender] -= usedAmount;
                 totalBananasBalance -= usedAmount;
                 return;
             } else {
-                depositedKongs[_id].lenders.push(Deposit(depositAmount, d.sender));
+                depositedKongs[_id].shareholders.push(Deposit(depositAmount, d.sender));
                 selectedAmount = resultingAmount;
                 bananaBalance[d.sender] -= depositAmount;
                 totalBananasBalance -= depositAmount;
@@ -167,24 +126,23 @@ contract BananaPool is DepositQueue {
             }
         }
         // if while loop does not return early, we don't have enough bananas.
-        revert("Not enough deposits");
+        revert("Not enough deposits.");
     }
 
     function calcReward(uint256 time) internal pure returns(uint256) {
         return BASE_RATE.mul(time).div(86400);
     }
 
-    function getBananaBalance(address _addr) external view returns(uint256) {
+    function erc20Balance(address _addr) external view override returns(uint256) {
         return bananaBalance[_addr];
     }
 
-    function getTotalBananas() external view returns(uint256) {
+    function totalErc20() external view override returns(uint256) {
         return totalBananasBalance;
     }
 
-    function getDepositedKong(uint256 _id) external view returns(Kong memory) {
+    function depositedKong(uint256 _id) external view returns(Kong memory) {
         return depositedKongs[_id];
     }
-
 
 }
--- test/hyksos-cyberkongz.js
+++ test/hyksos-cyberkongz.js
@@ -1,6 +1,6 @@
 const Kongz = artifacts.require("Kongz");
 const Bananas = artifacts.require("YieldToken");
-const Pool = artifacts.require("BananaPool");
+const Hyksos = artifacts.require("HyksosCyberkongz");
 
 
 web3.extend({
@@ -28,11 +28,11 @@ const assertRevert = async promise => {
 }
 
 
-contract("BananaPool test", async accounts => {
+contract("HyksosCyberkongz test", async accounts => {
   it("Sanity", async () => {
     const kongz = await Kongz.deployed();
     const bananas = await Bananas.deployed();
-    const pool = await Pool.deployed();
+    const hyksos = await Hyksos.deployed();
     assert.equal(await kongz.ownerOf(1001), accounts[0])
     await kongz.transferFrom(accounts[0], accounts[1], 1001, {from: accounts[0], gas: 1000000})
     assert.equal(await kongz.ownerOf(1001), accounts[1])
@@ -44,62 +44,62 @@ contract("BananaPool test", async accounts => {
     assert.equal((await bananas.balanceOf(accounts[1])).toString(10), web3.utils.toWei('1000', 'ether'));
   });
 
-  it("Deposit and withdraw bananas to pool", async () => {
+  it("Deposit and withdraw bananas to hyksos", async () => {
     const kongz = await Kongz.deployed();
     const bananas = await Bananas.deployed();
-    const pool = await Pool.deployed();
+    const hyksos = await Hyksos.deployed();
     assert.equal((await bananas.balanceOf(accounts[1])).toString(10), web3.utils.toWei('1000', 'ether'));
-    await bananas.approve(pool.address, web3.utils.toWei('500', 'ether'), {from: accounts[1]})
-    await assertRevert(pool.depositBananas(web3.utils.toWei('1000', 'ether'), { from: accounts[1]}))
-    await pool.depositBananas(web3.utils.toWei('500', 'ether'), { from: accounts[1]});
-    assert.equal((await pool.getBananaBalance(accounts[1])).toString(10), web3.utils.toWei('500', 'ether'));
+    await bananas.approve(hyksos.address, web3.utils.toWei('500', 'ether'), {from: accounts[1]})
+    await assertRevert(hyksos.depositErc20(web3.utils.toWei('1000', 'ether'), { from: accounts[1]}))
+    await hyksos.depositErc20(web3.utils.toWei('500', 'ether'), { from: accounts[1]});
+    assert.equal((await hyksos.erc20Balance(accounts[1])).toString(10), web3.utils.toWei('500', 'ether'));
     assert.equal((await bananas.balanceOf(accounts[1])).toString(10), web3.utils.toWei('500', 'ether'));
-    await pool.withdrawBananas({ from: accounts[1]});
-    assert.equal((await pool.getBananaBalance(accounts[1])).toString(10), "0");
+    await hyksos.withdrawErc20({ from: accounts[1]});
+    assert.equal((await hyksos.erc20Balance(accounts[1])).toString(10), "0");
     assert.equal((await bananas.balanceOf(accounts[1])).toString(10), web3.utils.toWei('1000', 'ether'));
   });
 
   it("Simple Kong deposit", async () => {
     const kongz = await Kongz.deployed();
     const bananas = await Bananas.deployed();
-    const pool = await Pool.deployed();
+    const hyksos = await Hyksos.deployed();
     
     console.log("transfer Kong to account 2, to avoid confusion")
     await kongz.transferFrom(accounts[1], accounts[2], 1001, {from: accounts[1], gas: 1e6})
     assert.equal(await kongz.ownerOf(1001), accounts[2])
     
     console.log("verify that deposit is not possible with empty deposit queue")
-    await assertRevert(pool.lendKong(1001, {from: accounts[2], gas: 1e6}))
+    await assertRevert(hyksos.depositNft(1001, {from: accounts[2], gas: 1e6}))
     
 
-    console.log("deposit 1000 bananas into the pool from account 2 and verify amount")
+    console.log("deposit 1000 bananas into the hyksos from account 2 and verify amount")
     assert.equal((await bananas.balanceOf(accounts[1])).toString(10), web3.utils.toWei('1000', 'ether'));
     assert.equal((await bananas.balanceOf(accounts[2])).toString(10), "0");
-    const bananasApprovalSummary = await bananas.approve(pool.address, web3.utils.toWei('1000', 'ether'), {from: accounts[1]})
-    const bananasDepositSummary = await pool.depositBananas(web3.utils.toWei('1000', 'ether'), { from: accounts[1]});
-    assert.equal((await pool.getBananaBalance(accounts[1])).toString(10), web3.utils.toWei('1000', 'ether'));
+    const bananasApprovalSummary = await bananas.approve(hyksos.address, web3.utils.toWei('1000', 'ether'), {from: accounts[1]})
+    const bananasDepositSummary = await hyksos.depositErc20(web3.utils.toWei('1000', 'ether'), { from: accounts[1]});
+    assert.equal((await hyksos.erc20Balance(accounts[1])).toString(10), web3.utils.toWei('1000', 'ether'));
     assert.equal((await bananas.balanceOf(accounts[1])).toString(10), web3.utils.toWei('0', 'ether'));
     
-    console.log("deposit a kong into the pool from account 1")
-    const kongApprovalSummary = await kongz.approve(Pool.address, 1001,  {from: accounts[2]});
-    const kongLendSummary = await pool.lendKong(1001, {from: accounts[2], gas: 1e6})
+    console.log("deposit a kong into the hyksos from account 1")
+    const kongApprovalSummary = await kongz.approve(Hyksos.address, 1001,  {from: accounts[2]});
+    const kongLendSummary = await hyksos.depositNft(1001, {from: accounts[2], gas: 1e6})
     
     console.log("verify that the Kong lender has received his reward")
     assert.equal((await bananas.balanceOf(accounts[2])).toString(10), web3.utils.toWei('80', 'ether'));
     
     console.log("verify that it's impossible to withdraw a kong before the due date")
     web3.evm.increaseTime(5 * 86400);
-    await assertRevert(pool.withdrawKong(1001, {from: accounts[2], gas: 1e6}))
+    await assertRevert(hyksos.withdrawNft(1001, {from: accounts[2], gas: 1e6}))
     
     console.log("withdraw a kong after the due date")
     web3.evm.increaseTime(5 * 86400 + 1);
-    const kongWithdrawalSummary = await pool.withdrawKong(1001, {from: accounts[2], gas: 1e6})
+    const kongWithdrawalSummary = await hyksos.withdrawNft(1001, {from: accounts[2], gas: 1e6})
 
     console.log("verify that the banana donor has received their reward.")
     assert((await bananas.balanceOf(accounts[1])) >= web3.utils.toBN(web3.utils.toWei('100', 'ether')));
 
-    console.log("verify there's a correct amount of bananas left on the pool")
-    assert((await pool.getBananaBalance(accounts[1])).toString(10) == web3.utils.toWei('920', 'ether'));
+    console.log("verify there's a correct amount of bananas left on the hyksos")
+    assert((await hyksos.erc20Balance(accounts[1])).toString(10) == web3.utils.toWei('920', 'ether'));
 
     console.log("Gas summary:\nKong deposit: %d\nKong withdrawal: %d\nBananas deposit: %d", 
                 kongApprovalSummary['receipt']['cumulativeGasUsed'] + kongLendSummary['receipt']['cumulativeGasUsed'],
@@ -110,38 +110,38 @@ contract("BananaPool test", async accounts => {
   it("Complex Kong deposits", async () => {
     const kongz = await Kongz.deployed();
     const bananas = await Bananas.deployed();
-    const pool = await Pool.deployed();
+    const hyksos = await Hyksos.deployed();
 
 
-    assert.equal((await pool.getTotalBananas()).toString(10), web3.utils.toWei('920', 'ether'));
+    assert.equal((await hyksos.totalErc20()).toString(10), web3.utils.toWei('920', 'ether'));
     console.log("920 / 80 = 11 more Kong deposits should be possible")
 
     for (let i = 0; i < 11; i++) {
-      await kongz.approve(Pool.address, 1001,  {from: accounts[2]});
-      await pool.lendKong(1001, {from: accounts[2], gas: 1e6})
+      await kongz.approve(Hyksos.address, 1001,  {from: accounts[2]});
+      await hyksos.depositNft(1001, {from: accounts[2], gas: 1e6})
       web3.evm.increaseTime(10 * 86400 + 1);
-      await pool.withdrawKong(1001, {from: accounts[2], gas: 1e6})
+      await hyksos.withdrawNft(1001, {from: accounts[2], gas: 1e6})
     }
-    console.log("13th deposit shouldn't be possible, (40 bananas left in the pool)")
-    assert.equal((await pool.getTotalBananas()).toString(10), web3.utils.toWei('40', 'ether'));
-    await assertRevert(pool.lendKong(1001, {from: accounts[2], gas: 1e6}))
+    console.log("13th deposit shouldn't be possible, (40 bananas left in the hyksos)")
+    assert.equal((await hyksos.totalErc20()).toString(10), web3.utils.toWei('40', 'ether'));
+    await assertRevert(hyksos.depositNft(1001, {from: accounts[2], gas: 1e6}))
 
-    console.log("share account 1 funds across multiple accounts to build a bigger deposit queue. Deposit the funds into the pool")
+    console.log("share account 1 funds across multiple accounts to build a bigger deposit queue. Deposit the funds into the hyksos")
     for (let i = 3; i < 20; i++) {
       await bananas.transfer(accounts[i], web3.utils.toWei('10', 'ether'), {from: accounts[1], gas: 1e6})
-      await bananas.approve(pool.address, web3.utils.toWei('10', 'ether'), {from: accounts[i]})
-      await pool.depositBananas(web3.utils.toWei('10', 'ether'), { from: accounts[i]});
-      assert.equal((await pool.getBananaBalance(accounts[i])).toString(10), web3.utils.toWei('10', 'ether'));
+      await bananas.approve(hyksos.address, web3.utils.toWei('10', 'ether'), {from: accounts[i]})
+      await hyksos.depositErc20(web3.utils.toWei('10', 'ether'), { from: accounts[i]});
+      assert.equal((await hyksos.erc20Balance(accounts[i])).toString(10), web3.utils.toWei('10', 'ether'));
       assert.equal((await bananas.balanceOf(accounts[i])).toString(10), "0");
     }
     
     console.log("Kong deposit should now be possible")
-    await kongz.approve(Pool.address, 1001,  {from: accounts[2]});
-    await pool.lendKong(1001, {from: accounts[2], gas: 1e6})
+    await kongz.approve(Hyksos.address, 1001,  {from: accounts[2]});
+    await hyksos.depositNft(1001, {from: accounts[2], gas: 1e6})
     
     console.log("withdraw Kong and verify that only accounts 3, 4, 5, 6 have received at least 12.5 bananas in rewards")
     web3.evm.increaseTime(10 * 86400 + 1);
-    await pool.withdrawKong(1001, {from: accounts[2], gas: 1e6})
+    await hyksos.withdrawNft(1001, {from: accounts[2], gas: 1e6})
     for (let i = 3; i < 7; i++) {
       assert(await bananas.balanceOf(accounts[i]) >= web3.utils.toBN(web3.utils.toWei('12.5', 'ether')), "No reward on account " + i);
     }
@@ -150,15 +150,15 @@ contract("BananaPool test", async accounts => {
     }
 
     console.log("Withdraw bananas of account 9 to make a gap in deposit queue. Send them to account 1")
-    const bananasWithdrawalSummary = await pool.withdrawBananas({ from: accounts[9]});
+    const bananasWithdrawalSummary = await hyksos.withdrawErc20({ from: accounts[9]});
     await bananas.transfer(accounts[1], web3.utils.toWei("10", "ether"), {from: accounts[9]});
     assert.equal((await bananas.balanceOf(accounts[9])).toString(10), "0");
     
     console.log("Deposit Kong once again and verify that addresses 7, 8, 10, 11, 12, 13, 14, 15 received rewards (14th deposit)")
-    const kongApprovalSummary = await kongz.approve(Pool.address, 1001,  {from: accounts[2]});
-    const kongLendSummary = await pool.lendKong(1001, {from: accounts[2], gas: 1e6})
+    const kongApprovalSummary = await kongz.approve(Hyksos.address, 1001,  {from: accounts[2]});
+    const kongLendSummary = await hyksos.depositNft(1001, {from: accounts[2], gas: 1e6})
     web3.evm.increaseTime(10 * 86400 + 1);
-    const kongWithdrawalSummary = await pool.withdrawKong(1001, {from: accounts[2], gas: 1e6})
+    const kongWithdrawalSummary = await hyksos.withdrawNft(1001, {from: accounts[2], gas: 1e6})
     for (const i of [7, 8, 10, 11, 12, 13, 14, 15]) {
       assert(await bananas.balanceOf(accounts[i]) >= web3.utils.toBN(web3.utils.toWei('12.5', 'ether')), "No reward on account " + i);
     }
