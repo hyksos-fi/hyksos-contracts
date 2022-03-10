69477a125f974b820e440e2a3aa96e1dc3ba1ab2
fylkir <fylkirh@protonmail.com>
2022-03-10 17:58:45 +0100

    migrate to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,7 +34,16 @@
              runs: 200,
            }
          }
-       }
+       },
+       {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          }
+        }
+      },
      ]
    }
  };
